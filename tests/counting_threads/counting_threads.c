5cd4c24f5701ed117ba0b42590197476c0c21e7e
Ori Shriqui <127379291+OriWarshavski@users.noreply.github.com>
2023-07-24 18:02:54 +0200

    fix counting threads + spin lock solution

--- tests/counting_threads/counting_threads.c
+++ tests/counting_threads/counting_threads.c
@@ -6,46 +6,46 @@
 #define IS_ODD(NUM) (NUM % 2)
 
 int count = 0;
-sem_t mutex;
+sem_t odd, even;
 
-// static void* counting_odd_v1(void* a)
-// {
-//     while(1) {
-//         sem_wait(&mutex);
-//         if(IS_ODD(count)) {
-//             printf("odd: %d\n", count);
-//             ++count;
-//         }
-//         sem_post(&mutex);
-//         if(count == N)
-//         {
-//             break;
-//         }
-//     }
-//     return NULL;
-// }
+static void* counting_odd_v1(void* a)
+{
+    while(1) {
+        sem_wait(&odd);
+        if(IS_ODD(count)) {
+            printf("odd: %d\n", count);
+            ++count;
+        }
+        sem_post(&even);
+        if(count == N)
+        {
+            break;
+        }
+    }
+    return NULL;
+}
 
-// static void* counting_even_v1(void* a)
-// {
-//     while(1) {
-//         sem_wait(&mutex);
-//         if(!IS_ODD(count)) {
-//             printf("even: %d\n", count);
-//             ++count;
-//         }
-//         sem_post(&mutex);
-//         if(count == N + 1)
-//         {
-//             break;
-//         }
-//     }
-//     return NULL;
+static void* counting_even_v1(void* a)
+{
+    while(1) {
+        sem_wait(&even);
+        if(!IS_ODD(count)) {
+            printf("even: %d\n", count);
+            ++count;
+        }
+        sem_post(&odd);
+        if(count == N + 1)
+        {
+            break;
+        }
+    }
+    return NULL;
 
-// }
+}
 
-pthread_cond_t even_cond = PTHREAD_COND_INITIALIZER;
-pthread_cond_t odd_cond = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t even_cond;
+pthread_cond_t odd_cond;
+pthread_mutex_t mtx;
 
 void* counting_odd_v2(void* arg) {
     for (int i = 1; i < N; i += 2) {
@@ -73,50 +73,93 @@ void* counting_even_v2(void* arg) {
     return NULL;
 }
 
+int printEven = 1;
 
-// static void test_v1();
+void* counting_odd_v3(void* arg) {
+    for (int i = 1; i < N; i += 2) {
+        while(printEven);
+        printf("odd: %d\n", i);
+        if(i < N) {
+            printEven = 1;
+        }
+    }
+    return NULL;
+}
+
+void* counting_even_v3(void* arg) {
+    for (int i = 0; i <= N; i += 2) {
+        while(!printEven);
+        printf("even: %d\n", i);
+        if(i < N) {
+            printEven = 0;
+        }
+    }
+    return NULL;
+}
+
+static void test_v1();
 static void test_v2();
+static void test3_spinLock();
 
 int main()
 {
-    /*test_v1();*/
+    test_v1();
     test_v2();
+    test3_spinLock();
+
 
     return 0;
 }
 
-// static void test_v1()
-// {
-//     pthread_t odd, even;
-//     sem_init(&mutex, 0, 1);
-//     pthread_create(&even, NULL, counting_even_v1, NULL);
-//     pthread_create(&odd, NULL, counting_odd_v1, NULL);
+static void test_v1()
+{
+    pthread_t oddT, evenT;
+    sem_init(&odd, 0, 1);
+    sem_init(&even, 0, 1);
+    pthread_create(&evenT, NULL, counting_even_v1, NULL);
+    pthread_create(&oddT, NULL, counting_odd_v1, NULL);
 
-//     pthread_join(odd, NULL);
-//     pthread_join(even, NULL);
-//     sem_destroy(&mutex);
+    pthread_join(oddT, NULL);
+    pthread_join(evenT, NULL);
+    sem_destroy(&odd);
+    sem_destroy(&even);
 
-//     if(count == 1001)
-// 	{
-// 		printf("Test V1 ......................................PASS\n");
-// 	}
-// 	else
-// 	{
-// 		printf("Test V1 ......................................FAIL\n");
-// 	}
-// }
+    if(count == 1001)
+	{
+		printf("Test V1 ......................................PASS\n");
+	}
+	else
+	{
+		printf("Test V1 ......................................FAIL\n");
+	}
+}
 
 static void test_v2()
 {
     pthread_t odd, even;
-    // pthread_mutex_init(&mtx, NULL);
-    // pthread_cond_init(&even_cond, NULL);
-    // pthread_cond_init(&odd_cond, NULL);
+    pthread_mutex_init(&mtx, NULL);
+    pthread_cond_init(&even_cond, NULL);
+    pthread_cond_init(&odd_cond, NULL);
 
     pthread_create(&even, NULL, counting_even_v2, NULL);
     pthread_create(&odd, NULL, counting_odd_v2, NULL);
 
     pthread_join(odd, NULL);
     pthread_join(even, NULL);
+
+	printf("Test V2 ......................................PASS\n");
+
+}
+
+static void test3_spinLock()
+{
+    pthread_t oddTh, evenTh;
+    pthread_create(&evenTh, NULL, counting_even_v3, NULL);
+    pthread_create(&oddTh, NULL, counting_odd_v3, NULL);
+
+    pthread_join(evenTh, NULL);
+    pthread_join(oddTh, NULL);
+
+	printf("Test V3 ......................................PASS\n");
 
 }
